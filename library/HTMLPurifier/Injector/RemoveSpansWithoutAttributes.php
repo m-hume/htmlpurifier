3304bbd2fde1744eb39a4decaf957c897730e465
m-hume <github@monkeyr.com>
2026-07-06 11:59:59 +0100

    Fix all SplObjectStorage deprecations and null array offset for PHP 8.5

--- library/HTMLPurifier/Injector/RemoveSpansWithoutAttributes.php
+++ library/HTMLPurifier/Injector/RemoveSpansWithoutAttributes.php
@@ -74,7 +74,7 @@ class HTMLPurifier_Injector_RemoveSpansWithoutAttributes extends HTMLPurifier_In
 
         if ($current instanceof HTMLPurifier_Token_End && $current->name === 'span') {
             // Mark closing span tag for deletion
-            $this->markForDeletion->attach($current);
+            $this->markForDeletion->offsetSet($current, null);
             // Delete open span tag
             $token = false;
         }
@@ -86,7 +86,7 @@ class HTMLPurifier_Injector_RemoveSpansWithoutAttributes extends HTMLPurifier_In
     public function handleEnd(&$token)
     {
         if ($this->markForDeletion->offsetExists($token)) {
-            $this->markForDeletion->detach($token);
+            $this->markForDeletion->offsetUnset($token);
             $token = false;
         }
     }
